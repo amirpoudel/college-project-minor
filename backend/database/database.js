2f079082dfc6b5ecb7a08b42289b0152b3991a90
amirpoudel <amirpoudel2058@gmail.com>
2022-08-04 17:01:34 +0545

    react

--- backend/database/database.js
+++ backend/database/database.js
@@ -5,74 +5,193 @@ require("dotenv").config();
 
 const password = process.env.DB_PASSWORD;
 const user = process.env.DB_USERNAME;
-const url  = `mongodb+srv://${user}:${password}@cluster0.azv1xi6.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${user}:${password}@cluster0.azv1xi6.mongodb.net/?retryWrites=true&w=majority`;
 
 
-function connectDatabase(){
-    
-    mongoose.connect(url).then(()=>{
+function connectDatabase() {
+
+    mongoose.connect(url).then(() => {
         console.log("Connection successful");
-    }).catch((err)=>{
+    }).catch((err) => {
         console.log(err);
     });
 }
 
+// -------------------------------------Inserting data-----------------------------------------------------
+
 // testing code for create admin 
-function createAdmin(){
-    const admin =  new models.adminModel({
-        firstName:"Amir",
-        middleName:" ",
-        lastName:"Poudel",
-        email:"amirpoudel2058@gmail.com",
-        password:"helloworld"
-    })
-    admin.save();
+async function createAdmin(firstName,middleName,lastName,email,password) {
+
+    try {
+        const admin = new models.adminModel({
+            firstName: firstName,
+            middleName: middleName,
+            lastName: lastName,
+            email: email,
+            password: password
+        })
+        await admin.save();
+    } catch (error) {
+        console.log(error)
+    }
+
+
 }
 
 //testing code for create user
-function createUser(){
-    const user = new models.userModel({
-        voterID:2020116280,
-        firstName:"Amir",
-        middleName:" ",
-        lastName:"Poudel",
-        email:"amirpoudel2058@gmail.com",
-        password:"helloworld"
-    })
-    user.save();
+async function createUser(voterID,firstName,middleName,lastName,email,password) {
+    try {
+        const user = new models.userModel({
+            voterID: voterID,
+            firstName: firstName,
+            middleName: middleName,
+            lastName: lastName,
+            email: email,
+            password: password
+        })
+        await user.save();
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
 //testing code for create candidate
-function createCandidate(){
-    const candidate = new models.candidateModel({
-        candidateID:541018,
-        firstName  :"Kp",
-        middleName :"",
-        lastName   :"Oli",
-        post       :"PM",
-        electionStatus : false,
-    })
+async function createCandidate(candidateID,firstName,middleName,lastName,post) {
+    try {
+        const candidate = new models.candidateModel({
+            candidateID: candidateID,
+            firstName: firstName,
+            middleName: middleName,
+            lastName: lastName,
+            post: post,
+            totalVote:0,
+            electionStatus: false,
+        })
 
-    candidate.save();
+        await candidate.save();
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
 //testing code for create vote
 
-function  createVote(){
-    const vote = new models.voteModel({
-        candidateID:541018,
-        vote:1
-    })
-    vote.save();
+async function createVote(candidateID) {
+    try {
+        const vote = new models.voteModel({
+            candidateID: 541018,
+            vote: 1
+        })
+        await vote.save();
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+// ------------------------------------------------------------Reading Data----------------------------------------------
+
+
+async function getDocuments() {
+    const result = await models.adminModel.find();
+    return result;
+
+}
+
+// finds all votes
+async function findAllVotes() {
+    const result = await models.voteModel.find();
+    return result;
+}
+
+//finds all candidate 
+async function findAllCandidates() {
+    const result = await models.candidateModel.find();
+    return result;
+}
+//finds candidate by candidateID
+
+async function findCandidate(candidateID) {
+    const result = await models.candidateModel.find({ candidateID: candidateID });
+    return result;
+}
+
+//finds all users
+
+async function findUsers() {
+    const result = await models.userModel.find();
+    return result;
+}
+
+//update data in models
+
+async function update_candidate_vote(id){
+        try {
+            await models.candidateModel.findOneAndUpdate({candidateID:id},
+                {$inc:{
+                    totalVote:1
+                }})
+            //console.log(updateVote);
+        } catch (error) {
+            console.log("Error! ! ! cannot update candidate vote")
+        }
 }
 
 
 
+// update candidate votes
 
-module.exports={
-    connectDatabase:connectDatabase,
-    createAdmin:createAdmin,
-    createUser:createUser,
-    createCandidate:createCandidate,
-    createVote:createVote
+async function updateCandidateVotes() {
+    
+    //function for find all votes
+    async function allVotes() {
+        const result = await findAllVotes();
+        return result;
+
+    }
+    //find Onecandidate
+    async function oneCandidate(candidateID){
+        const result = await findCandidate(candidateID);
+        return result;
+    }
+
+    const totalVotes = await allVotes();
+    //console.log(totalVotes)
+   ;
+   
+    totalVotes.forEach( async function(vote){
+        console.log(vote.candidateID);
+        //const candidate =  oneCandidate(vote.candidate);
+        //console.log(candidate.candidateID);
+        
+       await update_candidate_vote(vote.candidateID);
+        // console.log(candidate.candidateID);
+       
+    })
+    
+}
+
+module.exports = {
+    //function for inserting data
+    connectDatabase: connectDatabase,
+    createAdmin: createAdmin,
+    createUser: createUser,
+    createCandidate: createCandidate,
+    createVote: createVote,
+
+    //function for reading data
+    getDocuments: getDocuments,
+    findAllVotes: findAllVotes,
+    findAllCandidates: findAllCandidates,
+    findCandidate: findCandidate,
+    findUsers: findUsers,
+
+
+    //update candidate votes
+    updateCandidateVotes:updateCandidateVotes,
+
 }
--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,53 @@ const database = require('./database/database');
 const express = require('express');
 const app = express();
 
-const port = 8000;
+const port = 5000;
 
 
 database.connectDatabase();
 
-database.createAdmin();
-database.createUser();
-database.createCandidate();
-database.createVote();
+// database.createAdmin();
+// database.createUser();
+ //database.createCandidate();
+// let count = 0;
+// while(count<5){
+//     database.createVote();
+//     count++;
+// }
+ 
+
+/// testing code for find data
+
+// async function allVotes(){
+//     const allVotes = await database.findAllVotes();
+//      console.log(allVotes);
+    
+// }
+// allVotes();
+
+async function allCandidate(){
+    const allCandidate = await database.findAllCandidates();
+    console.log(allCandidate);
+}
+// allCandidate();
+
+async function candidate(candidateID){
+    const candidate = await database.findCandidate(candidateID);
+    console.log(candidate);
+}
+candidate(541018);
+
+async function allUsers(){
+    const users = await database.findUsers();
+    console.log(users);
+}
+allUsers();
+
+//Update candidate votes
+database.updateCandidateVotes();
 
 
+// Routing
 app.get("/",(req,res)=>{
     res.send("Hello world");
 })
